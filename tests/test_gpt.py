48e3a5cbd72afb18758e5c310b92b2c2a093d48a
christe6-osu <christe6@oregonstate.edu>
2025-06-30 19:12:41 -0700

    mock test_simple_gpt

--- tests/test_gpt.py
+++ tests/test_gpt.py
@@ -27,7 +27,7 @@ def test_simple_gpt(mocker):
     )
 
     mock_request = mocker.patch(
-        "src.gpt.client.create",
+        "src.gpt.g4f.client.create",
         return_value=mock_response.return_value,
     )
 
